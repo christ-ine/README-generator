3d7b38a8b38112fe78ea7a654dee0c406a3926f0
Christine Ross <christinejoyross@gmail.com>
2020-08-27 14:58:17 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
+const thenableWriteFile = util.promisify(fs.writeFile);
+function getReadMeOutput(answers) {
+    
+}
